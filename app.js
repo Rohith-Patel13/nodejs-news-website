de3f54e12a66ccfdd7bc9f2c6d741ea7a44e14fb
Rohith-Patel13 <rohithgithub999@gmail.com>
2024-05-23 22:43:54 +0530

    setup

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const morgan = require("morgan")
+const cors = require("cors")
+require("dotenv").config()
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+app.use(morgan("dev")); // Added morgan for logging HTTP requests
+
+app.get("*",async(requestObject,responseObject)=>{
+    console.log("Endpoint doesnot exist")
+    responseObject.status(404).send("Endpoint doesnot exist")
+})
+
+app.listen(4596,async()=>{
+    console.log("Server Connected to given Port")
+    try {
+        await mongoose.connect(process.env.MONGODB_URL)
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        console.log(error.message)
+    }
+});
