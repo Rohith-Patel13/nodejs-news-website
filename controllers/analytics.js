65717078c2a22a712187cf9d8f0c4ae05c1ce340
Rohith-Patel13 <rohithgithub999@gmail.com>
2024-05-25 17:41:19 +0530

    analytics

--- controllers/analytics.js
+++ controllers/analytics.js
@@ -1,9 +1,26 @@
+// controllers/analytics.js
 const Analytics = require("../models/analytics");
+const Comment = require("../models/comment");
+const User = require("../models/users");
 
 
 exports.createArticleView = async (requestObject, responseObject) => {
     try {
-        const newView = await Analytics.create(requestObject.body);
+        const { articleId } = requestObject.body;
+
+        // Count the number of comments for the article
+        const commentCount = await Comment.countDocuments({ articleId });
+
+        // Count the total number of registered users
+        const totalUserCount = await User.countDocuments();
+
+        // Create the analytics entry
+        const newView = await Analytics.create({
+            articleId,
+            viewCountOnArticle: commentCount,
+            totalRegisteredUsers: totalUserCount
+        });
+
         responseObject.status(201).send(newView);
     } catch (error) {
         responseObject.status(500).send({ error: error.message });
--- models/analytics.js
+++ models/analytics.js
@@ -2,9 +2,10 @@ const {Schema,model} = require('mongoose')
 
 const analyticsSchema = new Schema({
   articleId: { type: Schema.Types.ObjectId, ref: 'Article', required: [true,"articleId must not be empty"] },
-  userId: { type: Schema.Types.ObjectId, ref: 'User', required: [true,"userId must not be empty"] },
-  viewCount: { type: Number, default: 0 },
+  viewCountOnArticle: { type: Number, default: 0 },
+  totalRegisteredUsers:{type:Number,default:0}
 },{timestamps:true})
+
 
 const Analytics = model('Analytics', analyticsSchema)
 
